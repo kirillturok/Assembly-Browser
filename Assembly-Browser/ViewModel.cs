7f0e73dddbe7cec1aab16432e1de970913313fd0
Kiryl <kirillturok1@gmail.com>
2021-10-28 00:13:51 +0300

    Добавьте файлы проекта.

---
+++ Assembly-Browser/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly_Browser
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public List<NamespaceData> 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
