e92fc1b00bd14175c2a0100d36651535adb0694d
Kiryl <kirillturok1@gmail.com>
2021-10-29 00:30:22 +0300

    Added changing of the file name after choosing a file

--- Assembly-Browser/ViewModel/ButtonViewModel.cs
+++ Assembly-Browser/ViewModel/ButtonViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,17 +9,80 @@ using System.Windows;
 
 namespace Assembly_Browser.ViewModel
 {
-    class ButtonViewModel
+    class ButtonViewModel : INotifyPropertyChanged
     {
+        private string _fileName;
+        public string FileName
+        {
+            get
+            {
+                return _fileName;
+            }
+            set
+            {
+                _fileName = value;
+                OnPropertyChanged("FileName");
+            }
+        }
+
+
         public ButtonCommand ButtonCommand { get; set; }
         public ButtonViewModel()
         {
             ButtonCommand = new ButtonCommand(this);
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
         public void OnExecute()
         {
-            MessageBox.Show("Button is cliicked");
+            var fileDialog = new OpenFileDialog
+            {
+                Filter = "Assemblies|*.dll;*.exe",
+                Title = "Select assembly",
+                Multiselect = false
+            };
+
+            var isOpen = fileDialog.ShowDialog();
+
+            if(isOpen == null)
+            {
+                FileName = "File hasn't been chosen.";
+                return;
+            }
+
+            if (isOpen.Value)
+            {
+                FileName = fileDialog.FileName;
+                CreateTree(FileName);
+            }
+            //MessageBox.Show("Button is cliicked");
         }
+
+        private List<int> _namespaces;
+        public List<int> Namespaces { get; set; }
+
+        private void CreateTree(string FileName)
+        {
+            Namespaces = null;
+            try
+            {
+                //Namespaces=
+            }catch(Exception e)
+            {
+                FileName = e.Message;
+            }
+
+            OnPropertyChanged("Namespaces");
+        }
+
     }
 }
